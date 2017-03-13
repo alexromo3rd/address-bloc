a07bf82e8d14be6d7314e5178727074c6fecd7d9
Alex Romo III <alexromo3rd@gmail.com>
2017-03-12 22:43:24 -0700

    Initial commit

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,6 +1,9 @@
 require_relative '../models/address_book'
 
 RSpec.describe AddressBook do
+
+  let(:book) { AddressBook.new }
+
   describe "attributes" do
     it "responds to entries" do
       book = AddressBook.new
